b14a7eb03f0ea2e31577297eb5ed4527b502c1c7
ZZQ <zzgaming@list.ru>
2020-02-07 17:44:46 +1000

    Add search request by name

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+import request from "request";
+
 export type NpmSearchParams = {
     name?: string
-    keywords: string[]
-    ranking:  'optimal' |
+    keywords?: string[]
+    ranking?:  'optimal' |
         'maintenance' |
         'popularity' |
         'quality'
@@ -9,6 +11,16 @@ export type NpmSearchParams = {
 
 export default function npmSearch(search: NpmSearchParams): Promise<object> {
     return new Promise((resolve, reject) => {
-
+        request({
+            method: 'GET',
+            uri: 'https://npmjs.com/search?q=' + search.name,
+            headers: {
+                'Accept': 'application/json',
+                'x-requested-with': 'XMLHttpRequest',
+                'x-spiferack': 1
+            }
+        }, (err, res, body) => {
+           resolve(JSON.parse(body).objects)
+        })
     })
 }
