98ac8204b0d1a9b86eaeb8dc1f63347b590d32e0
ZZQ <zzgaming@list.ru>
2020-02-07 18:41:02 +1000

    Add RegistryPackage type

--- index.ts
+++ index.ts
@@ -1,26 +1,91 @@
-import request from "request";
+import request, {CoreOptions, UriOptions} from "request";
+import {URLSearchParams} from "url";
+import {Runtime} from "inspector";
+
+export type RegistryPackage = {
+    package: {
+        name: string
+        scope: string
+        version: string
+        description: string
+        keywords: string[]
+        date: {
+            ts: number
+            rel: string
+        }
+        links: {
+            npm: string
+            homepage?: string
+            repository?: string
+            bugs?: string
+        },
+        author?: {
+            name: string
+            email: string
+            useaname: string
+        }
+        publisher: {
+            name: string,
+            avatars: {
+                small: string
+                medium: string
+                large: string
+            }
+            created: {
+                ts: number | null
+                rel: string
+            },
+            email: string
+        },
+        maintainers: Array<{
+            username: string
+            email: string
+        }>
+        keywordsTruncated: boolean
+    }
+    score: {
+        final: number,
+        detail: {
+            quality: number
+            popularity: number
+            maintenance: number
+        }
+    }
+    searchScore: number
+}
 
 export type NpmSearchParams = {
     name?: string
     keywords?: string[]
-    ranking?:  'optimal' |
+    ranking?: 'optimal' |
         'maintenance' |
         'popularity' |
         'quality'
 }
 
-export default function npmSearch(search: NpmSearchParams): Promise<object> {
+export default function npmSearch(search: NpmSearchParams): Promise<Array<RegistryPackage>> {
     return new Promise((resolve, reject) => {
-        request({
-            method: 'GET',
-            uri: 'https://npmjs.com/search?q=' + search.name,
-            headers: {
-                'Accept': 'application/json',
-                'x-requested-with': 'XMLHttpRequest',
-                'x-spiferack': 1
-            }
-        }, (err, res, body) => {
-           resolve(JSON.parse(body).objects)
+        request(createRequestParams(search), (err, res, body) => {
+            if (err) reject(err);
+            resolve(JSON.parse(body).objects)
         })
     })
+}
+
+function createRequestParams(options: NpmSearchParams): UriOptions & CoreOptions { // UriOptions & CoreOptions from @types/request package
+    const searchParams = new URLSearchParams();
+
+    if (options.name)/*-----*/searchParams.set('q', options.name);
+    if (options.keywords)/*-*/searchParams.set('q', 'keywords:' + options.keywords.join(' '));
+    if (options.ranking)/*--*/searchParams.set('ranking', options.ranking);
+
+    return {
+        method: 'GET',
+        uri: 'http://npmjs.com/search?' + searchParams.toString(),
+        headers: {
+            'Accept': 'application/json',
+            'x-requested-with': 'XMLHttpRequest',
+            'x-spiferack': 1
+        }
+    }
 }
