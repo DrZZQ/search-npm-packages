21449ab0a3dfca9f2ad1b64af35e477e35374453
ZZQ <zzgaming@list.ru>
2020-02-11 22:29:39 +1000

    Move types to types.ts

---
+++ src/types.ts
@@ -0,0 +1,84 @@
+export type NpmRegistryPackage = {
+    name: string
+    scope: string
+    version: string
+    description: string
+    keywords: string[]
+    date: {
+        ts: number
+        rel: string
+    }
+    links: {
+        npm: string
+        homepage?: string
+        repository?: string
+        bugs?: string
+    },
+    author?: {
+        name: string
+        email: string
+        useaname: string
+    }
+    publisher: {
+        name: string,
+        avatars: {
+            small: string
+            medium: string
+            large: string
+        }
+        created: {
+            ts: number | null
+            rel: string
+        },
+        email: string
+    },
+    maintainers: Array<{
+        username: string
+        email: string
+    }>
+    keywordsTruncated: boolean
+}
+
+type NpmSearchResult = {
+    formData: {
+        search: {
+            q: {
+                value: string
+            }
+        }
+    },
+    objects: Array<{
+        package: NpmRegistryPackage
+        score: {
+            final: number,
+            detail: {
+                quality: number,
+                popularity: number,
+                maintenance: number
+            }
+        },
+        "searchScore": number
+    }>
+    total: number,
+    time: string,
+    pagination: {
+        perPage: number,
+        page: number
+    },
+    url: string,
+    user: null,
+    csrftoken: string,
+    notifications: [],
+    npmExpansions: string[],
+    isNpme: false
+}
+
+export type NpmSearchParams = {
+    name?: string
+    keywords?: string[]
+    ranking?: 'optimal' |
+        'maintenance' |
+        'popularity' |
+        'quality'
+    quantity?: number
+}
